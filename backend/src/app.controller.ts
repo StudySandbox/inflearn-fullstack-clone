0dfe29136da92d62fab936ae81f12871ea909ec0
Insu Hong <hinsu93@gmail.com>
2025-08-19 19:16:33 +0900

    BE-JWT인증 설정

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Request } from 'express';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { AccessTokenGuard } from './auth/guards/access-token.guard';
 
 @Controller()
 export class AppController {
@@ -9,5 +11,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('user-test')
+  @UseGuards(AccessTokenGuard)
+  testUser(@Req() req: Request) {
+    // AUTH_SECRET으로 해독한 유저 정보를 출력
+    console.log(req.user);
+    return 'test completed';
   }
 }
---
+++ backend/src/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+type JwtPayload = {
+  sub: string;
+  username: string;
+};
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-access-token', // access-token.guard.ts에 AuthGuard에 추가한 값과 동일해야 합니다.
+) {
+  constructor() {
+    // super를 활용해서 상속받은 PassportStrategy에 있는 constructor를 사용합니다.
+    super({
+      // 요청 헤더에 있는 'Authorization':'Bearer {accessToken}'을 자동으로 분석하는 역할
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+
+      // JWT의 만료를 무시할지 여부
+      ignoreExpiration: false,
+
+      // 암호화에 사용한 AUTH_SECRET
+      secretOrKey: process.env.AUTH_SECRET!,
+    });
+  }
+
+  // 토큰에 들어있는 정보가 유효한지 검증
+  validate(payload: JwtPayload) {
+    return payload;
+  }
+}
